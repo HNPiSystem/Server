7a4ffbd5593c932090d0c8c4f3982509883437e1
Seungwon Kim <closer527@gmail.com>
2013-11-15 20:34:54 +0900

    add some routes, create login method

--- Server.py
+++ Server.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify, url_for, redirect
 
 app = Flask(__name__)
 
+# class NetworkMng:
+#     def __init__(self, passwd):
+#         self.passwd = passwd
+#     def getPasswd(self):
+#         return self.passwd
 
+# Login with password
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Welcome to HNPi System'
 
+@app.route('/index')
+def index():
+    return redirect(url_for('hello_world'))
+
+@app.route('/login', methods=['POST'])
+def validatePW():
+    password = request.args.get('passwd')
+    # import hashlib
+    # pw = hashlib.md5()
+    # pw.update("secret")
+    # pw.digest()
+    if password == 'secret':
+        return jsonify({'result': 'Valid Password'})
+    else:
+        return jsonify({'result': 'Invalid Password'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
