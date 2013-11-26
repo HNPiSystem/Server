1d8675a47a1c6371257dfe6c3b0b652693a86be9
Seungwon Kim <closer527@gmail.com>
2013-11-26 19:42:03 +0900

    NetworkManager 클래스 추가 ; 액세스토큰, 비밀번호 관리용 (싱글톤 패턴 적용)

--- Server.py
+++ Server.py
@@ -4,10 +4,55 @@ from flask import Flask, request, jsonify, url_for, redirect
 
 app = Flask(__name__)
 
+
+def singleton(class_):
+    class class_w(class_):
+        _instance = None
+
+        def __new__(cls, *args, **kwargs):
+            if class_w._instance is None:
+                class_w._instance = super(class_w, cls).__new__(cls, *args, **kwargs)
+                class_w._instance._sealed = False
+            return class_w._instance
+
+        def __init__(self, *args, **kwargs):
+            if self._sealed:
+                return
+            super(class_w, self).__init__(*args, **kwargs)
+            self._sealed = True
+
+    class_w.__name__ = class_.__name__
+    return class_w
+
+
+@singleton
+class NetworkManager(object):
+    """
+    This is the network Manager class to manage the access token, system login password.
+    """
+    password = ''
+    access_token = ''
+
+    def __init__(self, passwd):
+        self.password = passwd
+        self.access_token = get_access_token()
+
+    @classmethod
+    def name(cls):
+        print cls.__name__
+
+    def getPassword(self):
+        return self.password
+
+    def getAccessToken(self):
+        return self.access_token
+
+
 # Login with password
 @app.route('/')
 def hello_world():
     return 'Welcome to HNPi System'
+
 
 @app.route('/index')
 def index():
@@ -50,14 +95,14 @@ def get_encrypted_pw():
     import hashlib
 
     pw = hashlib.md5()
-    pw.update("secret")
-    print pw.digest()
+    pw.update(x.getPassword())
     return pw.digest()
 
 
-def create_access_token():
+def get_access_token():
     """
     This is a method to create an access token for request
+    :rtype : str
     :return: access_token
     """
     # Todo : 서버가 실행될 때 무작위적으로 액세스토큰을 만드는 로직이 필요 ; 현재 일시적으로 'accessToken' 문자열 리턴
@@ -74,16 +119,23 @@ def validatePW():
     password = request.args.get('passwd')
     # encrypted_pw = get_encrypted_pw()
     # Todo : 외부에서 설정한 패스워드로 지정
-    # Todo : 패스워드가 맞아도 로직이 제대로 작동 안하는 것 해결
+
+    # access_token = get_access_token()
+    atoken = x.getAccessToken()
     if password == '1234':
-        return jsonify({'result': '%s' % create_access_token()})
+        return jsonify({'result': '1234'})
     else:
         return jsonify({'result': 'Invalid Password'})
 
 
 import loop_module
+
 p = Process(target=loop_module.loop, args=())
 p.start()
 
 if __name__ == '__main__':
+    x = NetworkManager(1234)
+    x.name()
+    print x.getAccessToken()
+    print x.getPassword()
     app.run(host='0.0.0.0')
